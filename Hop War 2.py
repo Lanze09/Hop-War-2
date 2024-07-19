cf46ebf1b6ad5eef9d3024c70147756a58331d23
Lanze09 <lanzeavellanoza@gmail.com>
2024-07-19 14:32:00 +0800

    Started actually coding it

--- Hop War 2.py
+++ Hop War 2.py
@@ -1,9 +1,12 @@
 from tkinter import *
 
+gameWidth = 900
+gameHeight = 700
 
 window = Tk()
-window.geometry('600x600')
+window.geometry(f'{gameWidth}x{gameHeight}')
 window.title("Hop War 2")
+window.resizable(False, False)
 
 window.update()
 window_width = window.winfo_width()
@@ -14,7 +17,10 @@ x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-#sample change
+window.config(background='#98F5F9')
+
+hop_Area = Canvas(window, background='black', width=gameWidth, height=gameHeight - gameHeight * 0.30)
+hop_Area.pack()
  
 
 
