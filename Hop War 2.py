2392f8b82fd76d18b11b10c2488e08dcbc878f86
Lanze09 <lanzeavellanoza@gmail.com>
2024-07-19 14:10:21 +0800

    First Commit

---
+++ Hop War 2.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+
+window = Tk()
+window.geometry('600x600')
+window.title("Hop War 2")
+
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+#sample change
+ 
+
+
+window.mainloop()
+
